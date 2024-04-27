c4cd018889eb8368c41c8917d9fb1c6eec2ae7a5
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-04-27 15:47:31 -0400

    troubleshoot routes access

--- index.js
+++ index.js
@@ -19,26 +19,26 @@ app.get("/", (req, res) => {
 });
 dotenv.config();
 
-app.use("/auth", userRouter);
-app.use("/data", dataRouter);
-app.use("/getConverstion", convRouter);
+// app.use("/auth", userRouter);
+// app.use("/data", dataRouter);
+// app.use("/getConverstion", convRouter);
 
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    const PORT = process.env.PORT || 2002;
+// mongoose
+//   .connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     const PORT = process.env.PORT || 2002;
 
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+//     app.listen(PORT, () => {
+//       console.log(`Server started on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
