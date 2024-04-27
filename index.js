a9dc1b746cfc8fd06dc811a20bd5351e4d2bd278
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-04-27 15:51:06 -0400

    trial trouble shoot api routes

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const { userRouter } = require("./routes/users.js");
-const { dataRouter } = require("./routes/data.js");
-const { convRouter } = require("./routes/conv.js");
+const express = require('express')
 
-const app = express();
-const mongoURI = process.env.MONGODB_URI;
-
-app.use(express.json());
-app.use(cors());
-
-const PORT = 4000;
-
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-dotenv.config();
-
-// app.use("/auth", userRouter);
-// app.use("/data", dataRouter);
-// app.use("/getConverstion", convRouter);
-
-// mongoose
-//   .connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//     const PORT = process.env.PORT || 2002;
-
-//     app.listen(PORT, () => {
-//       console.log(`Server started on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to MongoDB:", error);
-//   });
+const app = express()
+const PORT = 4000
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+  console.log(`API listening on PORT ${PORT} `)
+})
 
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 // Export the Express API
 module.exports = app
